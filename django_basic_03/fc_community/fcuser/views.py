b1aec745226e74ec50a280d24e85a6a983f76605
SM-it-now <ibb09@naver.com>
2021-04-16 00:11:20 +0900

    STATIC FILE ( +CDN )

---
+++ django_basic_03/fc_community/fcuser/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from .models import Fcuser
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
+        useremail = request.POST.get('useremail', None)
+
+
+        res_data = {}
+
+        if not (username and useremail and password and re_password):
+            res_data['error'] = '모든 값을 입력해야 합니다.'
+        if password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            fcuser = Fcuser(
+                username=username,
+                useremail=useremail,
+                password=make_password(password)
+            )
+
+            fcuser.save()
+
+        return render(request, 'register.html', res_data)
+
